81441a555d5922086c2ea062802f287308fc1ddf
idleYak <58395081+idleYak@users.noreply.github.com>
2021-10-27 01:27:57 +0300

    Update alter_sportcomplex.sql

--- task_2/scirpts/alter_sportcomplex.sql
+++ task_2/scirpts/alter_sportcomplex.sql
@@ -1,5 +1,4 @@
-
--- Формируем первичные ключи для созданных таблиц (пункт 1)
+-- form primary keys for the created tables (paragraph 1)
 ALTER TABLE kdct_equipment_accounting ADD CONSTRAINT pk_kdct_equipment_accounting PRIMARY KEY ( equipment_accounting_id );
 ALTER TABLE kdct_room_accounting ADD CONSTRAINT pk_kdct_room_accounting PRIMARY KEY ( room_accounting_id );
 ALTER TABLE kdct_training_shedule ADD CONSTRAINT pk_kdct_training_shedule PRIMARY KEY ( training_shedule_id );
@@ -11,42 +10,42 @@ ALTER TABLE kspo_group ADD CONSTRAINT pk_kspo_group PRIMARY KEY ( group_id );
 ALTER TABLE kspo_trainer ADD CONSTRAINT pk_kspo_trainer PRIMARY KEY ( trainer_id );
 
 
--- переименование некоторых объектов для более удобного вида (пункт 2)
+-- renaming some objects for a more convenient view (paragraph 2)
 ALTER TABLE kdct_training_shedule RENAME TO kdct_shedule;
 
--- переименование колонки в таблице (пункт 3)
+-- renaming a column in a table (paragraph 3)
 ALTER TABLE kspo_trainer RENAME COLUMN middle_name TO otchestvo;
 
 
--- добавление тестовой колонки для следующих манипуляций
+-- adding a test column for the following manipulations
 ALTER TABLE kspo_client ADD middle_name VARCHAR2(100 CHAR) NOT NULL;
 
--- блок колонки в неиспользуемую (пункт 4)
+-- column block to unused(paragraph 4)
 ALTER TABLE kspo_client SET UNUSED COLUMN middle_name;
 
--- удаление колонки (пункт 5)
+-- column deletion (paragraph 5)
 ALTER TABLE kspo_client DROP COLUMN middle_name;
--- Данные действия вызвали системную ошибку из-за юезра, поэтому пропишем иначе
+-- These actions caused a system error due to the user, so we will write it differently
 
 /*
 CREATE TABLE SYS.kspo_client2 as select * from kspo_client;
 alter table SYS.kspo_client2 SET UNUSED (middle_name);
 */
---используем вспомогательного пользователя
+--using a helper user
 CREATE TABLE anonymous.kspo_client2 as select * from kspo_client;
 ALTER TABLE anonymous.kspo_client2 SET UNUSED (middle_name);
---удаление столбца из новой таблицы
+-- deleting a column from a new table
 ALTER TABLE anonymous.kspo_client2 DROP (first_name);
 
 drop TABLE kspo_client2;
 
 
---конечный вариант (пункт 4-5)
+--Final version(paragraph 4-5)
 CREATE TABLE anonymous.kspo_client3 as select * from kspo_client;
 ALTER TABLE anonymous.kspo_client3 SET UNUSED (middle_name);
 ALTER TABLE anonymous.kspo_client3 DROP UNUSED COLUMNS;
 
---вариант для (пункт 6), удаление данных по условию стажа
+--version for(paragraph 6), deletion of data on the condition of seniority
 DELETE FROM kspo_trainer 
 WHERE age_experience < 2;
 
